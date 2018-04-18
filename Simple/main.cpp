62539ff007ec031bc755c138093c5a1196313b47
codesniper <akhilvaid21@gmail.com>
2018-04-19 01:18:36 +0530

    First time

---
+++ Simple/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include<stdio.h>
+#include<bits/stdc++.h>
+
+#define ll long long int
+#define li long int
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    cout<<"n is = "<<n<<'\n';
+
+    return 0;
+}
